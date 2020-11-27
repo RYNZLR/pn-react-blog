e1dc254b992f89b03c5dbd393c5467ad81838d97
lisa.dewaele <lisa.dewaele@student.howest.be>
2020-11-27 17:03:53 +0100

    Change tech every x seconds

--- src/components/display/Technologies.js
+++ src/components/display/Technologies.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+
+import techs from '../../assets/data/skills.json';
 
 export default function Technologies(){
+    
+    function getRandomTech(){
+        return techs.technologies[Math.floor(Math.random() * techs.technologies.length)];
+    }
 
-    const techColor = "#14C7FF"
+    const [tech, setTech] = useState(getRandomTech());
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTech(getRandomTech());
+        }, 10000)
+    })
+
+
+    const techColor = tech.color;
+    const techName = tech.name;
+
+
 
     return(
        <section className="tech">
            <p>Let's do some</p>
-           <em style={{ color: `${techColor}` }}>React</em>
+           <em style={{ color: `${techColor}` }}>{techName}</em>
        </section>
     );
 
