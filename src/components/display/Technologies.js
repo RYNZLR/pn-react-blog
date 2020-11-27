97121f485dc1b8197bb27592562c73d69b448dff
lisa.dewaele <lisa.dewaele@student.howest.be>
2020-11-27 11:04:08 +0100

    Add technologies header element

---
+++ src/components/display/Technologies.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Technologies(){
+
+    const techColor = "blue"
+
+    return(
+       <section className="tech">
+           <p>Let's do some</p>
+           <em style={{ color: `${techColor}` }}>REACT</em>
+       </section>
+    );
+
+}
--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,12 +1,22 @@
 import Navigation from './Navigation'
 import React from 'react';
 
+import Technologies from '../display/Technologies'
+
+import { useLocation } from 'react-router-dom'
+
 export default function Header(){
 
+    const location = useLocation().pathname;
+    const sizeName = location === "/" ? "large" : "small"
 
     return(
-        <header>
+        <header className={sizeName}>
             <Navigation/>
+
+            {
+                sizeName === "large" ? <Technologies/> : ""
+            }
         </header>
     );
 
