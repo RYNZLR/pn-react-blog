0cd37557907b979b45927f8fd60a47e5b2504728
lisa.dewaele <lisa.dewaele@student.howest.be>
2020-11-27 17:46:00 +0100

    Add react word cycle

--- src/components/display/Technologies.js
+++ src/components/display/Technologies.js
@@ -1,31 +1,33 @@
 import React, { useState, useEffect} from 'react';
+import ReactRotatingText from 'react-rotating-text'
 
 import techs from '../../assets/data/skills.json';
 
 export default function Technologies(){
     
-    function getRandomTech(){
-        return techs.technologies[Math.floor(Math.random() * techs.technologies.length)];
+    const list = techs.technologies.map(t => t.name);
+    const colorList = techs.technologies.map(t => t.color);
+    
+
+    const [item, setItem] = useState(colorList.length-1)
+
+    function changeColor(){
+        if(item == colorList.length-1){
+            setItem(0);
+        }else{
+            setItem(item+1);
+        }
+        console.log(item);
     }
-
-    const [tech, setTech] = useState(getRandomTech());
-
-    useEffect(() => {
-        setTimeout(() => {
-            setTech(getRandomTech());
-        }, 10000)
-    })
-
-
-    const techColor = tech.color;
-    const techName = tech.name;
-
-
 
     return(
        <section className="tech">
            <p>Let's do some</p>
-           <em style={{ color: `${techColor}` }}>{techName}</em>
+           <em>
+               
+            <ReactRotatingText items={list} color={colorList[item]} pause={7000} emptyPause={500} onTypingStart={changeColor}/>
+               
+            </em>
        </section>
     );
 
